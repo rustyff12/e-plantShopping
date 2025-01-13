42307ebb233bd1ebb4b068a7746d245223fead5f
Russell Frankpitt <75209707+rustyff12@users.noreply.github.com>
2025-01-13 19:08:18 +0100

    CartItem.jsx fix, minor css changes

--- src/ProductList.jsx
+++ src/ProductList.jsx
@@ -1,12 +1,17 @@
-import React, { useState, useEffect } from "react";
+import { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
 import "./ProductList.css";
 import CartItem from "./CartItem";
 import { addItem } from "./CartSlice";
 
 function ProductList() {
 	const [showCart, setShowCart] = useState(false);
-	const [showPlants, setShowPlants] = useState(false); // State to control the visibility of the About Us page
-	const [addedToCart, setAddedToCart] = useState({});
+	const cartItems = useSelector((state) => state.cart.items);
+	const dispatch = useDispatch();
+
+	const getTotalItemCount = () => {
+		return cartItems.reduce((total, item) => total + item.quantity, 0);
+	};
 
 	const plantsArray = [
 		{
@@ -231,47 +236,47 @@ function ProductList() {
 			],
 		},
 	];
+
 	const styleObj = {
 		backgroundColor: "#4CAF50",
 		color: "#fff!important",
 		padding: "15px",
 		display: "flex",
 		justifyContent: "space-between",
-		alignIems: "center",
+		alignItems: "center",
 		fontSize: "20px",
 	};
+
 	const styleObjUl = {
 		display: "flex",
 		justifyContent: "space-between",
 		alignItems: "center",
 		width: "1100px",
 	};
+
 	const styleA = {
 		color: "white",
 		fontSize: "30px",
 		textDecoration: "none",
 	};
+
 	const handleCartClick = (e) => {
 		e.preventDefault();
 		setShowCart(true); // Set showCart to true when cart icon is clicked
 	};
+
 	const handlePlantsClick = (e) => {
 		e.preventDefault();
-		setShowPlants(true); // Set showAboutUs to true when "About Us" link is clicked
-		setShowCart(false); // Hide the cart when navigating to About Us
+		setShowCart(false); // Hide the cart when navigating to Plants
 	};
 
 	const handleContinueShopping = (e) => {
 		e.preventDefault();
-		setShowCart(false);
+		setShowCart(false); // Hide the cart when continuing shopping
 	};
 
 	const handleAddToCart = (product) => {
 		dispatch(addItem(product));
-		setAddedToCart((prevState) => ({
-			...prevState,
-			[product.name]: true, // Set the product name as key and value as true to indicate it's added to cart
-		}));
 	};
 
 	return (
@@ -297,20 +302,12 @@ function ProductList() {
 				</div>
 				<div style={styleObjUl}>
 					<div>
-						{" "}
-						<a
-							href="#"
-							onClick={(e) => handlePlantsClick(e)}
-							style={styleA}>
+						<a href="#" onClick={handlePlantsClick} style={styleA}>
 							Plants
 						</a>
 					</div>
 					<div>
-						{" "}
-						<a
-							href="#"
-							onClick={(e) => handleCartClick(e)}
-							style={styleA}>
+						<a href="#" onClick={handleCartClick} style={styleA}>
 							<h1 className="cart">
 								<svg
 									xmlns="http://www.w3.org/2000/svg"
@@ -328,11 +325,14 @@ function ProductList() {
 										d="M42.3,72H221.7l-26.4,92.4A15.9,15.9,0,0,1,179.9,176H84.1a15.9,15.9,0,0,1-15.4-11.6L32.5,37.8A8,8,0,0,0,24.8,32H8"
 										fill="none"
 										stroke="#faf9f9"
-										stroke-linecap="round"
-										stroke-linejoin="round"
-										stroke-width="2"
+										strokeLinecap="round"
+										strokeLinejoin="round"
+										strokeWidth="2"
 										id="mainIconPathAttribute"></path>
 								</svg>
+								<span className="cart_quantity_count">
+									{getTotalItemCount()}
+								</span>
 							</h1>
 						</a>
 					</div>
@@ -347,22 +347,26 @@ function ProductList() {
 									textAlign: "center",
 									marginTop: "10px",
 								}}>
-								<div>{category.category}</div>
+								{category.category}
 							</h1>
-							<div className="product-list">
+							<ul className="product-list">
 								{category.plants.map((plant, plantIndex) => (
 									<div
-										className="product-card"
-										key={plantIndex}>
+										key={plantIndex}
+										className="product-card">
 										<img
 											className="product-image"
 											src={plant.image}
-											alt={plant.name}
-										/>
-										<div className="product-title">
+											alt={plant.name}></img>
+										<h3 className="product-title">
 											{plant.name}
-										</div>
-
+										</h3>
+										<p className="product-description">
+											{plant.description}
+										</p>
+										<h4 className="product-cost">
+											{plant.cost}
+										</h4>
 										<button
 											className="product-button"
 											onClick={() =>
@@ -372,7 +376,7 @@ function ProductList() {
 										</button>
 									</div>
 								))}
-							</div>
+							</ul>
 						</div>
 					))}
 				</div>
